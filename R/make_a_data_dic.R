fa3783c1c7d5ee31ec7d0706d8c2dc7102f0f272
L. Zhang <lyzhang10@gmail.com>
2019-05-31 21:31:10 +1200

    add summary in data dic

--- R/make_a_data_dic.R
+++ R/make_a_data_dic.R
@@ -10,7 +10,9 @@
 #' temp_df <-
 #'   data.frame(a = 1:10,
 #'              b = NA,
-#'              e = c(letters[1:8], NA, NA))
+#'              d = c(1:9, NA),
+#'              e = c(letters[1:8], NA, NA),
+#'              stringsAsFactors = FALSE)
 #' x <- make_a_data_dic(temp_df)
 
 #' @importFrom magrittr %>%
@@ -25,8 +27,13 @@ make_a_data_dic <- function(a_df, var_of_interest = 1:dim(a_df)[2])
    dplyr::mutate(var_type = purrr::map_chr(b_df, typeof)) %>%
    dplyr::mutate(no_of_unique_values = purrr::map_int(b_df, function(x) length(unique(x)))) %>%
    dplyr::mutate(var_values = purrr::map(b_df, unique)) %>%
-   dplyr::mutate(sample_values = purrr::map(var_values, function(x) {if(length(x) <= 10) return(x); sample(x, 10)})) %>%
-   dplyr::select(var_names, var_type, no_of_unique_values, sample_values) %>%
+   dplyr::mutate(sample_values = purrr::map(var_values, function(x) {if(length(x) <= 10) return(x);
+                                                                     sample(x, 10)})) %>%
+   dplyr::mutate(min_Q1_Q2_Q3_max = purrr::map(b_df, function(x) {if(!is.numeric(x)) return(NA);
+        c(min(x, na.rm = TRUE), quantile(x, 0.25, na.rm = TRUE),
+        quantile(x, 0.5, na.rm = TRUE), quantile(x, 0.75, na.rm = TRUE),
+        max(x, na.rm = TRUE))})) %>%
+   dplyr::select(var_names, var_type, no_of_unique_values, sample_values, min_Q1_Q2_Q3_max) %>%
    dplyr::arrange(var_names)
 
  return(re_df)
