be4408601cbefec7660636b63e287f3282885cef
Larry Zhang <lzhang@wprdrstudio01.stats.govt.nz>
2020-10-17 12:21:33 +1300

    improve on find_df_key_info

--- R/find_df_key_info.R
+++ R/find_df_key_info.R
@@ -20,7 +20,11 @@ find_df_key_info <- function(a_df)
   dplyr::mutate(type = purrr::map_chr(a_df, typeof),
                 no_of_unique_rows = purrr::map_int(a_df, function(x) length(unique(x))),
                 no_of_rows = dim(a_df)[1],
-                no_of_NAs = purrr::map_int(a_df, function(x) sum(is.na(x)))) %>%
+                no_of_NAs = purrr::map_int(a_df, function(x) sum(is.na(x))),
+                min_width = purrr::map_dbl(a_df, function(x) min(stringr::str_length(x), na.rm = TRUE)),
+                max_width = purrr::map_dbl(a_df, function(x) max(stringr::str_length(x), na.rm = TRUE))) %>%
+  dplyr::mutate(min_width = ifelse(is.infinite(min_width), NA_real_, min_width),
+                max_width = ifelse(is.infinite(max_width), NA_real_, max_width)) %>%
   dplyr::arrange(vari_names)
  return(re_df)
 }
